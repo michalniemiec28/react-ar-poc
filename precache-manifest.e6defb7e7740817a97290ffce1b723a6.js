00f612c5b8f5b0c393fc84a0a3e1c582d6975d3c
Michał Niemiec <michal.niemiec@billennium.com>
2020-10-20 15:19:43 +0200

    Updates

---
+++ precache-manifest.e6defb7e7740817a97290ffce1b723a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9b2431642fbfb25e510fe7f0451310",
+    "url": "/react-ar-poc/index.html"
+  },
+  {
+    "revision": "6febee1c1cb30f227fb9",
+    "url": "/react-ar-poc/static/css/main.6fbad2f6.chunk.css"
+  },
+  {
+    "revision": "702ebae036d660888e18",
+    "url": "/react-ar-poc/static/js/2.82145e8e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-ar-poc/static/js/2.82145e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6febee1c1cb30f227fb9",
+    "url": "/react-ar-poc/static/js/main.1641090f.chunk.js"
+  },
+  {
+    "revision": "6b24a1c154ab18fb24cf",
+    "url": "/react-ar-poc/static/js/runtime-main.1e2222ba.js"
+  }
+]);
